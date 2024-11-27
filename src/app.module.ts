24f049d67d831c34e0a3bb8000f24be5abd02206
Kaleeswaran V <vkalees64@gmail.com>
2024-11-27 16:13:55 +0530

    feat: mongo db atlus connection update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,20 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      url: 'mongodb://localhost:27017/nest-task-app',
-      database: 'nest-task-app',
+      url: process.env.MONGO_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      ssl: false,
+      synchronize: true,
       useUnifiedTopology: true,
-      useNewUrlParser: true,
     }),
     UsersModule,
   ],
@@ -23,3 +25,23 @@ import { UsersModule } from './users/users.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+// Local Database
+// TypeOrmModule.forRoot({
+//       type: 'mongodb',
+//       url: 'mongodb://localhost:27017/nest-task-app',
+//       database: 'nest-task-app',
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       ssl: false,
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//     }),
+
+// Remote Database
+// TypeOrmModule.forRoot({
+//   type: 'mongodb',
+//   url: 'mongodb+srv://ahmer:Yahoo786@cluster0.2iz16.mongodb.net/mbe-erp?retryWrites=true&w=majority',
+//   synchronize: true, // Automatically creates database schema based on your entities (use carefully in production)
+//   useUnifiedTopology: true,
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+// });
